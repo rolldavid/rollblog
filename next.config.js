467c01b53a84836e943e329e940e7241b39bda5f
rolldavid <rolldev@proton.me>
2022-11-21 22:24:15 -0600

    config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
-    scrollRestoration: true,
     serverComponentsExternalPackages: [
       "prisma",
       "@prisma/client",
